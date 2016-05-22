0c00fdeef75d281c2d80e86c273fb759a27157a8
Kyle Buzsaki <kbuzsaki@ucsd.edu>
2016-05-22 01:44:19 -0700

    Made StaticFileHandler aware of directories

--- server/handlers.go
+++ server/handlers.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"github.com/kbuzsaki/httpserv/http"
 	"io/ioutil"
+	"os"
 	"path"
+	"strings"
 )
 
 // sample hello world server and handler
@@ -41,19 +43,50 @@ type StaticFileHandler struct {
 }
 
 func (h StaticFileHandler) Handle(request http.Request) http.Response {
-	var response http.Response
-	response.Protocol = http.HttpOneDotOne
-
 	filePath := path.Join(h.BasePath, request.Path)
+
+	info, err := os.Stat(filePath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return http.MakeErrorResponse(http.StatusNotFound, err)
+		} else {
+			return http.MakeErrorResponse(http.StatusInternalError, err)
+		}
+	} else if info.IsDir() {
+		return h.serveDirectory(request, filePath)
+	} else {
+		return h.serveFile(request, filePath)
+	}
+}
+
+func (h StaticFileHandler) serveFile(request http.Request, filePath string) http.Response {
 	bytes, err := ioutil.ReadFile(filePath)
 
 	if err != nil {
-		response.Status = http.StatusNotFound
-		response.Body = err.Error()
+		return http.MakeErrorResponse(http.StatusInternalError, err)
 	} else {
-		response.Status = http.StatusOk
-		response.Body = string(bytes)
+		return http.MakeSimpleResponse(string(bytes))
+	}
+}
+
+func (h StaticFileHandler) serveDirectory(request http.Request, filePath string) http.Response {
+	if !strings.HasSuffix(request.Path, "/") {
+		return http.MakeRedirectResponse("/" + filePath + "/")
 	}
 
-	return response
+	entries, err := ioutil.ReadDir(filePath)
+	if err != nil {
+		return http.MakeErrorResponse(http.StatusInternalError, err)
+	}
+
+	body := "<h1>Contents of " + request.Path + "</h1>"
+
+	body += "<ul>"
+	for _, entry := range entries {
+		href := request.Path + entry.Name()
+		body += "<li><a href=\"" + href + "\">" + entry.Name() + "</a></li>"
+	}
+	body += "</ul>"
+
+	return http.MakeSimpleResponse(body)
 }
